91ab605949f03a8f1f3b50b73359cb5b467e27c9
dexer <norton@e1.ru>
2022-07-12 14:49:45 +0500

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,22 @@
-import { MainPage } from '../../pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import { MainPage, FavoritesPage, LoginPage, NotFoundPage, RoomPage } from '../../pages';
 import { PLACES } from '../../';
 
 interface IAppProps {
   places: typeof PLACES;
 }
 
-export const App = ({ places }: IAppProps): JSX.Element => <MainPage places={places} />;
+export const App = ({ places }: IAppProps) => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRoute.Root} element={<MainPage places={places} />} />
+      <Route path={AppRoute.Login} element={<LoginPage />} />
+      <Route path={AppRoute.Favorites} element={<FavoritesPage />} />
+      <Route path={AppRoute.Offer} element={<RoomPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
