279adc6ba4e3c9b3f5e15eded30ffa1207804f5b
dexer <norton@e1.ru>
2022-07-15 15:40:32 +0500

    Исправляет ошибку линтера

--- project/src/components/places-list/places-list.tsx
+++ project/src/components/places-list/places-list.tsx
@@ -7,6 +7,7 @@ interface IPlacesListProps {
 }
 
 const PlacesList = ({ places }: IPlacesListProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [selectedCard, setSelectedCard] = useState<number | null>(null);
   return (
     <div className="cities__places-list places__list tabs__content">
