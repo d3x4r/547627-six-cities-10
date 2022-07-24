b37b5d6b26abaf3ea44ac08818d37f6df0367feb
dexer <norton@e1.ru>
2022-07-24 15:05:49 +0500

    Добавляет отображение предложений по выбранному городу

--- project/src/components/cities/cities.tsx
+++ project/src/components/cities/cities.tsx
@@ -1,42 +1,48 @@
-import { PlacesList } from '../../components';
+import { PlacesList } from '..';
+import { CitiesEmpty } from '../cities-empty';
 import { IOffer } from '../../types/offer';
 import { Map } from '../map';
+import { useAppSelector } from '../../hooks';
 
 interface ICitiesProps {
   places: IOffer[],
 }
 
 const Cities = ({ places }: ICitiesProps) => {
-  const placesCount = places.length;
+  const { offers, selectedCity } = useAppSelector((state) => state);
+
   return (
     <div className="cities">
-      <div className="cities__places-container container">
-        <section className="cities__places places">
-          <h2 className="visually-hidden">Places</h2>
-          <b className="places__found">{placesCount} places to stay in Amsterdam</b>
-          <form className="places__sorting" action="#" method="get">
-            <span className="places__sorting-caption">Sort by</span>
-            <span className="places__sorting-type" tabIndex={0}>
-              Popular
-              <svg className="places__sorting-arrow" width="7" height="4">
-                <use xlinkHref="#icon-arrow-select"></use>
-              </svg>
-            </span>
-            <ul className="places__options places__options--custom places__options--opened">
-              <li className="places__option places__option--active" >Popular</li>
-              <li className="places__option" tabIndex={0}>Price: low to high</li>
-              <li className="places__option" tabIndex={0}>Price: high to low</li>
-              <li className="places__option" tabIndex={0}>Top rated first</li>
-            </ul>
-          </form>
-          <PlacesList places={places} />
-        </section>
-        <div className="cities__right-section">
-          <section className="cities__map map">
-            <Map places={places} />
-          </section>
-        </div>
-      </div>
+      {offers.length === 0 ? <CitiesEmpty name={selectedCity} /> :
+        (
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{offers.length} places to stay in {selectedCity}</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-type" tabIndex={0}>
+                  Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
+                  </svg>
+                </span>
+                <ul className="places__options places__options--custom places__options--opened">
+                  <li className="places__option places__option--active" >Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
+                </ul>
+              </form>
+              <PlacesList places={offers} />
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map">
+                <Map places={places} />
+              </section>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
