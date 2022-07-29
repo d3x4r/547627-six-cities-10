81a697dd4c49d0c5ce6d4053cab1a63d82947c7c
dexer <norton@e1.ru>
2022-07-30 00:27:50 +0500

    Добавляет отображение статуса авторизации в шапке, с соответствующей функциональностью

--- project/src/components/navigation/navigation.tsx
+++ project/src/components/navigation/navigation.tsx
@@ -1,21 +1,48 @@
-const Navigation = () => (
-  <nav className="header__nav">
-    <ul className="header__nav-list">
-      <li className="header__nav-item user">
-        <a className="header__nav-link header__nav-link--profile" href="#">
-          <div className="header__avatar-wrapper user__avatar-wrapper">
-          </div>
-          <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-          <span className="header__favorite-count">3</span>
-        </a>
-      </li>
-      <li className="header__nav-item">
-        <a className="header__nav-link" href="#">
-          <span className="header__signout">Sign out</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../hooks';
+import { AuthorizationStatus, AppRoute } from '../../const';
+import { useAppDispatch } from '../../hooks';
+import { logoutAction } from '../../store/api-actions';
+
+const Navigation = () => {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+  const dispatch = useAppDispatch();
+
+  const onLogoutClick = () => {
+    dispatch(logoutAction());
+  };
+
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        {authorizationStatus === AuthorizationStatus.Auth ? (
+          <>
+            <li className="header__nav-item user">
+              <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                <div className="header__avatar-wrapper user__avatar-wrapper">
+                </div>
+                <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                <span className="header__favorite-count">3</span>
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <a className="header__nav-link" onClick={onLogoutClick}>
+                <span className="header__signout">Sign out</span>
+              </a>
+            </li>
+          </>
+        ) : (
+          <li className="header__nav-item user">
+            <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+              </div>
+              <span className="header__login">Sign in</span>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
