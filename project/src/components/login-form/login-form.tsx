3018c1550dda26dc578124a95937ab60a28d163e
dexer <norton@e1.ru>
2022-07-30 00:26:29 +0500

    Добавляет форму авторизации

--- project/src/components/login-form/login-form.tsx
+++ project/src/components/login-form/login-form.tsx
@@ -1,18 +1,50 @@
-const LoginForm = () => (
-  <section className="login">
-    <h1 className="login__title">Sign in</h1>
-    <form className="login__form form" action="#" method="post">
-      <div className="login__input-wrapper form__input-wrapper">
-        <label className="visually-hidden">E-mail</label>
-        <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
-      </div>
-      <div className="login__input-wrapper form__input-wrapper">
-        <label className="visually-hidden">Password</label>
-        <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
-      </div>
-      <button className="login__submit form__submit button" type="submit">Sign in</button>
-    </form>
-  </section>
-);
+import { useState } from 'react';
+import { useAppDispatch } from '../../hooks';
+import { loginAction } from '../../store/api-actions';
+
+const LoginForm = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const dispatch = useAppDispatch();
+
+  const onFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    dispatch(loginAction({ email: formData.email, password: formData.password }));
+  };
+
+  return (
+    <section className="login">
+      <h1 className="login__title">Sign in</h1>
+      <form
+        className="login__form form"
+        onSubmit={onFormSubmit}
+      >
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">E-mail</label>
+          <input
+            className="login__input form__input"
+            type="email" name="email"
+            placeholder="Email"
+            required
+            value={formData.email}
+            onChange={(evt) => setFormData({ ...formData, email: evt.target.value })}
+          />
+        </div>
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">Password</label>
+          <input
+            className="login__input form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+            value={formData.password}
+            onChange={(evt) => setFormData({ ...formData, password: evt.target.value })}
+          />
+        </div>
+        <button className="login__submit form__submit button" type="submit">Sign in</button>
+      </form>
+    </section>
+  );
+};
 
 export default LoginForm;
--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -1,19 +1,28 @@
 import { Header, LoginForm, LocationsItem } from '../../components';
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from '../../hooks';
+import { AuthorizationStatus, AppRoute } from '../../const';
 
-const LoginPage = () => (
+const LoginPage = () => {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
 
-  <div className="page page--gray page--login">
-    <Header />
-    <main className="page__main page__main--login">
-      <div className="page__login-container container">
-        <LoginForm />
-        <section className="locations locations--login locations--current">
-          <LocationsItem name="Amsterdam" />
-        </section>
-      </div>
-    </main>
-  </div>
-);
+  if (authorizationStatus === AuthorizationStatus.Auth) {
+    return <Navigate to={AppRoute.Root} />;
+  }
 
+  return (
+    <div className="page page--gray page--login">
+      <Header />
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <LoginForm />
+          <section className="locations locations--login locations--current">
+            <LocationsItem name="Amsterdam" />
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+};
 export default LoginPage;
 
