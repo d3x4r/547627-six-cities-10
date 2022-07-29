d6a8c8c0b65a736e6ffdaf068897c980c0555919
dexer <norton@e1.ru>
2022-07-30 00:24:49 +0500

    Добавляет проверку на авторизацию

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -3,8 +3,11 @@ import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AppDispatch, State } from '../types/state.js';
 import { IOffer } from '../types/offer';
-import { loadOffers, setDataLoadedStatus } from './action';
-import { APIRoute } from '../const';
+import { AuthData } from '../types/auth-data';
+import { UserData } from '../types/user-data';
+import { loadOffers, setDataLoadedStatus, setAuthorizationStatus } from './action';
+import { APIRoute, AuthorizationStatus } from '../const';
+import { saveToken, dropToken } from '../services/token';
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch,
@@ -17,5 +20,47 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
     const { data } = await api.get<IOffer[]>(APIRoute.Hotels);
     dispatch(loadOffers(data));
     dispatch(setDataLoadedStatus(false));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch,
+  state: State,
+  extra: AxiosInstance
+}>(
+  'user/checkAuth',
+  async (_arg, { dispatch, extra: api }) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(setAuthorizationStatus(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
+    }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch,
+  state: State,
+  extra: AxiosInstance
+}>(
+  'user/login',
+  async ({ email, password }, { dispatch, extra: api }) => {
+    const { data: { token } } = await api.post<UserData>(APIRoute.Login, { email, password });
+    saveToken(token);
+    dispatch(setAuthorizationStatus(AuthorizationStatus.Auth));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch,
+  state: State,
+  extra: AxiosInstance
+}>(
+  'user/logout',
+  async (_arg, {dispatch, extra: api}) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
   },
 );
