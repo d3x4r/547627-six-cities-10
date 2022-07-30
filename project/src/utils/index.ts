a99a05b7924f516c2185570666062f9513de8073
dexer <norton@e1.ru>
2022-07-30 15:55:49 +0500

    Добавляет функцию для сортировки комментариев

--- project/src/utils/index.ts
+++ project/src/utils/index.ts
@@ -1,7 +1,19 @@
 import { Rating, IOffer } from '../types/offer';
+import { IComment } from '../types/comment';
 
 export const getRatingWidth = (rating: Rating): number => rating ? 20 * Number(rating) : 0;
 
 export const sortOfferByPriceASC = (offers: IOffer[]) => [...offers].sort(({ price: firstPrice }, { price: secondPrice }) => firstPrice - secondPrice);
 export const sortOfferByPriceDESC = (offers: IOffer[]) => [...offers].sort(({ price: firstPrice }, { price: secondPrice }) => secondPrice - firstPrice);
 export const sortOfferByRatingDESC = (offers: IOffer[]) => [...offers].sort(({ rating: firstRating }, { rating: secondRating }) => Number(secondRating) - Number(firstRating));
+
+export const sortCommentsByDate = (first:IComment, second:IComment) => {
+  if (first.date < second.date) {
+    return 1;
+  }
+
+  if (first.date > second.date) {
+    return -1;
+  }
+  return 0;
+};
