8c0fc70ee5ccf65f256a5eea38cd13523568f4f4
dexer <norton@e1.ru>
2022-07-30 16:09:14 +0500

    Рефакторинг компонента с предложениями неподалёку

--- project/src/components/near-places/near-places.tsx
+++ project/src/components/near-places/near-places.tsx
@@ -1,31 +1,18 @@
-import { useEffect } from 'react';
 import { PlaceCard } from '../place-card';
 import { CardType } from '../place-card/const';
-import { useAppSelector, useAppDispatch } from '../../hooks';
-import { loadNearPlacesAction } from '../../store/api-actions';
+import { IOffer } from '../../types/offer';
 
-interface INearPlaces {
-  offerId: number;
+type NearPlacesPropsTypes = {
+  offers: IOffer[]
 }
 
-const NearPlaces = ({ offerId }: INearPlaces) => {
-  const { nearPlaces, currentOffer } = useAppSelector((state) => state);
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(loadNearPlacesAction(offerId));
-  }, [currentOffer]);
-
-  if (!nearPlaces) {
-    return <>No NearPlaces Loaded</>;
-  }
-  return (
-    <section className="near-places places">
-      <h2 className="near-places__title">Other places in the neighbourhood</h2>
-      <div className="near-places__list places__list">
-        {nearPlaces.map((place) => <PlaceCard key={place.id} place={place} cardType={CardType.nearPlace} />)}
-      </div>
-    </section>
-  );
-};
+const NearPlaces = ({ offers }: NearPlacesPropsTypes) => (
+  <section className="near-places places">
+    <h2 className="near-places__title">Other places in the neighbourhood</h2>
+    <div className="near-places__list places__list">
+      {offers.map((place) => <PlaceCard key={place.id} place={place} cardType={CardType.nearPlace} />)}
+    </div>
+  </section>
+);
 
 export default NearPlaces;
